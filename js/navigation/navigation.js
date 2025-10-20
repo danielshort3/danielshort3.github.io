288175889e57496911dd25e5252b99675448e30c
Daniel Short <143550027+danielshort3@users.noreply.github.com>
2025-10-19 18:05:17 -0600

    Fix hamburger menu on mobile

--- js/navigation/navigation.js
+++ js/navigation/navigation.js
@@ -83,6 +83,7 @@
         const open = menu.classList.toggle('open');
         burger.setAttribute('aria-expanded', open);
         document.body.classList.toggle('menu-open', open);
+        document.documentElement.classList.toggle('menu-open', open);
 
         if (open) {
           prevFocus = document.activeElement;
